fcb028f6216a70abd52726253e470a3343fef092
Tom Claessens <claessens.tom@gmail.com>
2022-06-04 12:49:16 +0200

    feat(hacs): updates custom components

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-25487c89.js')")();
+  new Function("import('/hacsfiles/frontend/main-150a7578.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-25487c89.js';
+  el.src = '/hacsfiles/frontend/main-150a7578.js';
   el.type = 'module';
   document.body.appendChild(el);
 }
